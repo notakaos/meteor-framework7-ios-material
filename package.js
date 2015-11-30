ac298beecb34ebb5ecfbfc2a250b383a527bcbe2
niravanu <nirvan@gmail.com>
2015-11-30 15:25:53 +0800

    Update package.js

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: 'nobutakaoshiro:framework7-ios-material',
+  name: 'niravanu:framework7-ios-material',
   version: '1.0.2',
   summary: 'Framework7 (v1.3.5) with Meteor. You can choose iOS or Android theme.',
-  git: 'https://github.com/nobutakaoshiro/meteor-framework7-ios-material.git',
+  git: 'https://github.com/niravanu/meteor-framework7-ios-material.git',
   documentation: 'README.md'
 });
 
